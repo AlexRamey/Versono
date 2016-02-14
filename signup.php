13ea02dea264e1725f95ef47aac426a6200abf52
michaelkochub <michaelkochub@gmail.com>
2016-02-13 19:25:55 -0500

    Fixed links, changed fonts/colors, added Lawn photos

--- signup.php
+++ signup.php
@@ -261,7 +261,8 @@
 								<!-- Copyright -->
 									<div id="copyright">
 										<ul class="links">
-											<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+											<li>&copy; 2016 Versono, Inc. All rights reserved.</li>
+											<li>Kochubeevsky Ramey Lee</li>
 										</ul>
 									</div>
 							</div>

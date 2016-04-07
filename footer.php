d3b52fcefc2146617c7ca5b3583560216bffbf71
AlexRamey <alex.b.ramey@gmail.com>
2016-04-07 12:43:03 -0400

    1. Removed excessive footer padding, which was stylistically criticezed by TAs in MS1 feedback

--- footer.php
+++ footer.php
@@ -1,17 +1,12 @@
 <!-- Footer -->
 <div id="footer-wrapper">
 	<section id="footer" class="container">
-		<div class="row">
-			<div class="12u">
-
-				<!-- Copyright -->
-					<div id="copyright">
-						<ul class="links">
-							<li>&copy; 2016 Versono, Inc. All rights reserved.</li>
-							<li>Kochubeevsky Ramey Lee</li>
-						</ul>
-					</div>
-			</div>
+		<!-- Copyright -->
+		<div id="copyright">
+			<ul class="links">
+				<li>&copy; 2016 Versono, Inc. All rights reserved.</li>
+				<li>Kochubeevsky Ramey Lee</li>
+			</ul>
 		</div>
 	</section>
 </div>
